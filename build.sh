ec8e4eb20303529310ed9a5d12d71375016276b5
Nick <nick@nicnak.net>
2026-05-01 07:19:23 +1200

    Complier flags to strip debug symbols

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # 64 but Intel Windows
-env GOOS=windows GOARCH=amd64 go build -o bin/Win64/testpdfgen.exe testpdfgen.go
+env GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o bin/Win64/testpdfgen.exe testpdfgen.go
 
 # # 64 bit Intel Mac
-env GOOS=darwin GOARCH=amd64 go build -o bin/OSX-Intel/testpdfgen testpdfgen.go
+env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o bin/OSX-Intel/testpdfgen testpdfgen.go
 
 # # Apple Silicon
-env GOOS=darwin env GOARCH=arm64 go build -o bin/OSX-AppleSilicon/testpdfgen testpdfgen.go
-env GOOS=darwin env GOARCH=arm64 go build -o testpdfgen testpdfgen.go
+env GOOS=darwin env GOARCH=arm64 go build -ldflags="-s -w" -o bin/OSX-AppleSilicon/testpdfgen testpdfgen.go
+env GOOS=darwin env GOARCH=arm64 go build -ldflags="-s -w" -o testpdfgen testpdfgen.go
 
